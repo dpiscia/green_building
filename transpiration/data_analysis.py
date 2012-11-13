fix average

transpiration/data_analysis.py
@@ -24,8 +24,8 @@ import data_functions as df
 import model_functions as mf
 import data_plot
 import numpy as np
-data_in = datetime(2010,6,22,10,00,0)
-data_fin = datetime(2010,6,29,10,05,0)
+data_in = datetime(2010,6,24,10,00,0)
+data_fin = datetime(2010,6,25,10,05,0)
 dati = df.query_db('greenhouse.db','data',data_in,data_fin)
 
 
@@ -35,18 +35,20 @@ tra_weight = mf.transpiration_from_balance(dati['peso_balanca'],300,2260000)
 
 delta_peso = np.diff(dati['peso_balanca'])
 fr,lista_irr,lista_irr_free = mf.find_irrigation_point(delta_peso,dati['data'])
-lista_night = mf.remove_no_solar_point(dati['rad_int_sup_solar'])
+lista_night = mf.remove_no_solar_point(dati['rad_int_sup_solar'],10)
 
 #data_plot.plot_time_data_2_y_axis(dati['data'][lista_fin],tra_P_M[lista_fin],'tra Penman',tran_weight,'trans weight')
 
-lista_fin = mf.lista_mod(lista_irr,lista_irr_free)
-lista = list(set(lista_irr+ lista_night))
-tran_weight = mf.transpiration_from_balance_irr(dati['peso_balanca'],300,2260000,lista) 
 
-min_avg = 12 
-  
-tra_P_M_avg,time_P_M = df.avg(tra_P_M[lista_fin],lista_fin,min_avg)
-tra_weigh_avg,time_weight = df.avg(tran_weight,lista_fin,min_avg)
+lista_no = list(set(lista_irr+ lista_night))
+#runlista_fin = mf.lista_mod(len(dati),lista_no)
+tran_weight,lista_yes = mf.transpiration_from_balance_irr(dati['peso_balanca'],300,2260000,lista_no) 
+
+min_avg = 24 
+
+
+tra_weigh_avg,time_weight = df.avg(tran_weight,lista_yes,min_avg)
+tra_P_M_avg,time_P_M = df.avg(tra_P_M,lista_yes,min_avg)
 data_plot.plot_time_data_2_y_same_axis(dati['data'][time_P_M],tra_P_M_avg,'tra Penman',tra_weigh_avg,'trans weight')
 RMSE = df.RMSE(tra_P_M_avg,tra_weigh_avg)
 print "RMSE is", RMSE

transpiration/model_functions.py
@@ -148,11 +148,11 @@ def lambda_constant(T):
     __lambda__ = 0.646 + 0.00064*T
     return __lambda__
 
-def lista_mod(lista,lista_in):
-    ''' remove item preceding an irrigation point'''
+def lista_mod(max_value,lista_in):
+    ''' return list with valid point'''
     lista_final = []
-    for i in (lista_in):
-        if not(i+1 in lista):
+    for i in range(0,max_value):
+        if not(i in lista_in):
             lista_final.append(i)
     
     return lista_final
@@ -176,10 +176,10 @@ def net_solar_radiation(Is,Rs,K,LAI):
         
     return Rn
     
-def remove_no_solar_point(Is):
+def remove_no_solar_point(Is,value):
     __lista__ = []
     for i in range(len(Is)):
-        if Is[i] <=0:
+        if Is[i] <= value:
             __lista__.append(i)
     return __lista__
         
@@ -286,13 +286,15 @@ def transpiration_from_balance_irr(weight,time_diff,L,irrigation_list):
         -L evaporization enthalpy (J kg^-1)
         '''
         
-
+    lista = []
     tran = []
     #__transpiration__ = np.diff(weight)*L/time_diff
     for i in range(len(weight)-1):
         if ((i+1 in irrigation_list) or (i in irrigation_list) ):
             print "irrigation point or drenage"
+            tran.append(0)
         else:
             tran.append((weight[i]-weight[i+1])*L/time_diff)
-    return tran
+            lista.append(i)
+    return tran,lista
     
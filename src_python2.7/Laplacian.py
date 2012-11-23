fe575244d75b6a251746712e5904419216580d76
unknown <dpiscia@A1398.irta.es>
2012-11-23 15:35:39 +0100

    add unsteady term to Laplacian solver

--- src_python2.7/Laplacian.py
+++ src_python2.7/Laplacian.py
@@ -10,13 +10,37 @@ import numpy as np
 
 x = 1
 y = 10
+
 T =  np.zeros(shape = (x,y))
 T0 = np.zeros(shape = T.shape) 
+T3 = np.zeros(shape = (x,y,2))
+T3[:,:,0] = 21
+
+def solver_unsteady(T,flux0,flux1,time):
+
+    rho = 1.225
+    density = 1000
+    alpha = 5
+    T_out = 5
+    deltaT = 60
+    k = 1
+    dy = 1
+    T[0,0,time] = (flux0 +  (k/dy)*T[0,0+1,time] + alpha*T_out + rho*density*dy*T[0,0,time-1]/deltaT   )/(1 + alpha + rho*density*dy/deltaT) 
+#    T[0,1] =   (T[0,0] + T[0,2])/2
+#    T[0,2] = (T[0,1] + T[0,3])/2
+    T[0,1:-1,time] = ( (k/dy)*(T[0,2:,time]+T[0,:-2,time]) + rho*density*dy*T[0,1:-1,time-1]/deltaT)/ (2 + rho*density*dy/deltaT)
+    T[0,-1,time] = 20
+    return T[0]
+
+
 def solver(T,flux0,flux1):
+
+    rho = 1.225
+    density = 1000
     
     k = 1
     dy = 1
-    T[0,0] = (flux0 +  (k/dy)*T[0,0+1] + 5*10)/(1+5)
+    T[0,0] = (flux0 +  (k/dy)*T[0,0+1] + 5*10   )/(1+5) 
 #    T[0,1] =   (T[0,0] + T[0,2])/2
 #    T[0,2] = (T[0,1] + T[0,3])/2
     T[0,1:-1] = (T[0,2:]+T[0,:-2])/2
